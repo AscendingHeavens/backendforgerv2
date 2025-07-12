5af9b9038693821cb00168279715c2117a00a0bf
Sarvesh-shinde23 <shindesarvesh023@gmail.com>
2025-07-12 23:05:04 +0530

    bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-import { ThemeProvider } from "@/components/Theme-provider";
+
 import Footer from "@/components/Footer";
 
 const geistSans = Geist({
